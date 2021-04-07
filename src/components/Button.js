e19fd6ef7adeca1bdbbec31f2d2ed88a730f2ba5
Alex Hungerford <alex@gmail.com>
2021-04-06 18:07:56 -0700

    button & onClick

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
     return (
-        <button style={{ backgroundColor: color }}
-        className='btn'>
+        <button
+            onClick={onClick} 
+            style={{ backgroundColor: color }}
+            className='btn'
+        >
             {text}
         </button>
     )
@@ -13,9 +16,11 @@ Button.defaultProps = {
     color: 'steelblue'
 }
 
+// notice the capitalization
 Button.propTypes = {
     text: PropTypes.string,
-    color: PropTypes.string
+    color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button;
