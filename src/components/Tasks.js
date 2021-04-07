309fa43d619eb9717d806a368a1741f8177f9af2
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2021-04-06 23:09:08 -0700

    demo useState in local function

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,26 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctor Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'School Meeting',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Grocery Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false
-    }
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctor Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'School Meeting',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Grocery Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false
+        }
+    ])
     return (
         <>
             {tasks.map((task) =>(<h3 key={task.id}>{task.text}</h3>))}
