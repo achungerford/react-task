6554f26c9ed4a76fa6f4c3a193d9d4fe2ba7d811
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2021-04-06 22:01:43 -0700

    tasks array

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,23 @@
-const tasks = []
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'School Meeting',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Grocery Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false
+    }
+]
 
 const Tasks = () => {
     return (
