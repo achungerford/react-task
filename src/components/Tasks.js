144128b6c6803f3575fd13556e88b52c67c37ecd
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2021-05-26 12:37:55 -0700

    onDelete fn created

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,14 @@
 import Task from './Task'
 
-const Tasks = ({ tasks }) => {
-    return (
-      <>
-        {tasks.map((task) => (
-        <Task key={task.id} task={task} />
-        ))}
-      </>
-    )
+const Tasks = ({ tasks, onDelete }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+      <Task key={task.id} task={task}
+      onDelete={onDelete} />
+      ))}
+    </>
+  )
 }
 
 export default Tasks
